b0229d59020a27c9543ad251ec9c11f9202c0e73
Joe Bethke <a9q25zz@mmm.com>
2019-08-07 14:01:01 -0500

    Updating for new app config key formats

--- storage-adapter/WebService/Runtime/Config.cs
+++ storage-adapter/WebService/Runtime/Config.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Azure.IoTSolutions.StorageAdapter.WebService.Runtime
     /// <summary>Web service configuration</summary>
     public class Config : IConfig
     {
-        private const string COSMOS_CONNECTION_STRING_APP_CONFIG_KEY = "cosmos-odin-mt-proc";
+        private const string COSMOS_CONNECTION_STRING_APP_CONFIG_KEY = "cosmosconnectionstring";
         private const string APPLICATION_KEY = "StorageAdapter:";
         private const string PORT_KEY = APPLICATION_KEY + "webservicePort";
         private const string STORAGE_TYPE_KEY = APPLICATION_KEY + "storageType";
