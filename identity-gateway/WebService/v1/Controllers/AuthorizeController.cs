d40afbd56769522087cb7555d9e9a8417be90550
Andrew Schmidt <a90q9zz@mmm.com>
2019-08-22 22:17:31 -0500

    added the guid check

--- identity-gateway/WebService/v1/Controllers/AuthorizeController.cs
+++ identity-gateway/WebService/v1/Controllers/AuthorizeController.cs
@@ -104,7 +104,8 @@ namespace IdentityGateway.Controllers
                     List<UserTenantModel> tenantList = await this._userTenantContainer.GetAllAsync(tenantInput);
                     Console.Write("Got TenantList:\n");
                     Console.WriteLine(tenantList);
-                    if (String.IsNullOrEmpty(authState.tenant))
+                    Guid tenantGuid; 
+                    if (String.IsNullOrEmpty(authState.tenant) || !Guid.TryParse(authState.tenant, out tenantGuid))
                     {
                         // authState has no tenant, so we should use either the User's last used tenant, or the first tenant available to them
                         // Create a UserSettingsInput for the purpose of finding the LastUsedTenant setting for this user
