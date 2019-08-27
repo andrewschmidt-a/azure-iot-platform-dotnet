9393a153f51884c966adb2f3322f9491b6fc2170
Sean Dubiel <a7gtkzz@mmm.com>
2019-08-27 09:39:43 -0500

    Added Delete api and made async changes to the TableStorage class

--- tenant-manager/WebService/Controllers/TenantController.cs
+++ tenant-manager/WebService/Controllers/TenantController.cs
@@ -33,9 +33,10 @@ namespace tenant_manager.Controllers
 
         // POST api/tenant
         [HttpPost]
-        public string Post()
+        public async Task<string> PostAsync()
         {
             /* Creates a new tenant */
+
 
             // Load variables from app config
             string subscriptionId = this._config["Global:subscriptionId"];
@@ -46,13 +47,15 @@ namespace tenant_manager.Controllers
             var secretTasks = new Task<string>[] {
                 this.keyVaultHelper.getSecretAsync("storageAccountConnectionString"),
                 this.keyVaultHelper.getSecretAsync("createIotHubWebHookUrl"),
-                this.keyVaultHelper.getSecretAsync("updateFunctionsWebHookUrl")
+                this.keyVaultHelper.getSecretAsync("updateFunctionsWebHookUrl"),
+                this.keyVaultHelper.getSecretAsync("deleteIotHubWebHookUrl")
             };
             Task.WaitAll(secretTasks);
             
             string storageAccountConnectionString = secretTasks[0].Result;
             string createIotHubWebHookUrl = secretTasks[1].Result;
             string updateFunctionsWebHookUrl = secretTasks[2].Result;
+            string deleteIotHubWebHookUrl = secretTasks[3].Result;
 
             // Generate new tenant information
             string tenantGuid = Guid.NewGuid().ToString();
@@ -61,9 +64,11 @@ namespace tenant_manager.Controllers
             string twinChangeCollectionName = "twin-change-" + tenantGuid.Substring(0, 8);
             string lifecycleCollectionName = "lifecycle-" + tenantGuid.Substring(0, 8);
 
+
+
             // Create a new tenant and save it to table storage
             var tenant = new TenantModel(tenantGuid, iotHubName, telemetryCollectionName);
-            TenantTableHelper.WriteNewTenantToTableAsync(storageAccountConnectionString, "tenant", tenant);
+            await TenantTableHelper.WriteNewTenantToTableAsync(storageAccountConnectionString, "tenant", tenant);
 
             // Trigger run book to create a new IoT Hub
             HttpClient client = new HttpClient();
@@ -79,17 +84,17 @@ namespace tenant_manager.Controllers
                 telemetryEventHubConnString = this._config["TenantManagerService:telemetryEventHubConnString"],
                 twinChangeEventHubConnString = this._config["TenantManagerService:twinChangeEventHubConnString"],
                 lifecycleEventHubConnString = this._config["TenantManagerService:lifecycleEventHubConnString"],
-                appConfigConnectionString = (Environment.GetEnvironmentVariable("appConfigConnectionString", EnvironmentVariableTarget.User)),
+                appConfigConnectionString = this._config["appConfigConnectionString"],
                 setAppConfigEndpoint = this._config["TenantManagerService:setAppConfigEndpoint"],
                 token = authToken
             };
 
             var bodyContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
-            client.PostAsync(createIotHubWebHookUrl, bodyContent);
-
+            var response = await client.PostAsync(createIotHubWebHookUrl, bodyContent);
             // Trigger run book to update the azure functions
             var requestBody2 = new
-            {   
+            {
+                type = "create",
                 tenantId = tenantGuid,
                 resourceGroup = rgName,
                 automationAccountName = this._config["TenantManagerService:automationAccountName"],
@@ -110,7 +115,7 @@ namespace tenant_manager.Controllers
             };
 
             bodyContent = new StringContent(JsonConvert.SerializeObject(requestBody2), Encoding.UTF8, "application/json");
-            client.PostAsync(updateFunctionsWebHookUrl, bodyContent);
+            await client.PostAsync(updateFunctionsWebHookUrl, bodyContent);
 
             return "Your tenant is being created. Your tenant GUID is: " + tenantGuid;
         }
@@ -128,6 +133,92 @@ namespace tenant_manager.Controllers
             TenantModel tenant = TenantTableHelper.ReadTenantFromTableAsync(storageAccountConnectionString, "tenant", tenantId).Result;            
 
             return tenant;
+        }
+
+
+
+         // DELETE api/tenantready/<tenantId>
+        [HttpDelete("{tenantId}")]
+        public async Task DeleteAsync(string tenantId)
+        {
+            string subscriptionId = this._config["Global:subscriptionId"];
+            string rgName = this._config["Global:resourceGroup"];
+            string location = this._config["Global:location"];
+
+            // Load secrets from key vault
+            var secretTasks = new Task<string>[] {
+                this.keyVaultHelper.getSecretAsync("storageAccountConnectionString"),
+                this.keyVaultHelper.getSecretAsync("updateFunctionsWebHookUrl"),
+                this.keyVaultHelper.getSecretAsync("deleteIotHubWebHookUrl")
+            };
+            Task.WaitAll(secretTasks);
+
+            string storageAccountConnectionString = secretTasks[0].Result;
+            string updateFunctionsWebHookUrl = secretTasks[1].Result;
+            string deleteIotHubWebHookUrl = secretTasks[2].Result;
+
+            // Load the tenant from table storage
+            TenantModel tenant = TenantTableHelper.ReadTenantFromTableAsync(storageAccountConnectionString, "tenant", tenantId).Result;
+            await TenantTableHelper.DeleteEntityAsync(storageAccountConnectionString, "tenant", tenant);
+
+            HttpClient client = new HttpClient();
+            var authToken = this.tokenHelper.GetServicePrincipleToken();
+
+
+            // Gather tenant information
+            string tenantGuid = tenantId;
+            string iotHubName = "iothub-" + tenantGuid.Substring(0, 8);
+            string telemetryCollectionName = "telemetry-" + tenantGuid.Substring(0, 8);
+            string twinChangeCollectionName = "twin-change-" + tenantGuid.Substring(0, 8);
+            string lifecycleCollectionName = "lifecycle-" + tenantGuid.Substring(0, 8);
+
+            //trigger delete iothub runbook
+            var requestBody = new
+            {
+                tenantId = tenantGuid,
+                iotHubName = iotHubName,
+                location = location,
+                subscriptionId = subscriptionId,
+                resourceGroup = rgName,
+                telemetryEventHubConnString = this._config["TenantManagerService:telemetryEventHubConnString"],
+                twinChangeEventHubConnString = this._config["TenantManagerService:twinChangeEventHubConnString"],
+                lifecycleEventHubConnString = this._config["TenantManagerService:lifecycleEventHubConnString"],
+                appConfigConnectionString = this._config["appConfigConnectionString"],
+                setAppConfigEndpoint = this._config["TenantManagerService:setAppConfigEndpoint"],
+                token = authToken
+            };
+
+            var bodyContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+
+            var response = await client.PostAsync(deleteIotHubWebHookUrl, bodyContent);
+
+            //trigger remove bindings from azure functions runbook
+            var requestBody2 = new
+            {
+                type = "delete",
+                tenantId = tenantGuid,
+                resourceGroup = rgName,
+                automationAccountName = this._config["TenantManagerService:automationAccountName"],
+                cosmosConnectionSetting = this._config["TenantManagerService:cosmosConnectionSetting"],
+                telemetryFunctionUrl = this._config["TenantManagerService:telemetryFunctionUri"],
+                twinChangeFunctionUrl = this._config["TenantManagerService:twinChangeFunctionUri"],
+                lifecycleFunctionUrl = this._config["TenantManagerService:lifecycleFunctionUri"],
+                telemetryFunctionName = this._config["TenantManagerService:telemetryFunctionName"],
+                twinChangeFunctionName = this._config["TenantManagerService:twinChangeFunctionName"],
+                lifecycleFunctionName = this._config["TenantManagerService:lifecycleFunctionName"],
+                telemetryCollectionName = telemetryCollectionName,
+                twinChangeCollectionName = twinChangeCollectionName,
+                lifecycleCollectionName = lifecycleCollectionName,
+                storageAccount = this._config["StorageAccount:name"],
+                databaseName = this._config["TenantManagerService:databaseName"],
+                tableName = this._config["TenantManagerService:tableName"],
+                token = authToken
+            };
+
+            bodyContent = new StringContent(JsonConvert.SerializeObject(requestBody2), Encoding.UTF8, "application/json");
+            await client.PostAsync(updateFunctionsWebHookUrl, bodyContent);
+
         }
     }
 }
--- tenant-manager/WebService/Helpers/TenantTableHelper.cs
+++ tenant-manager/WebService/Helpers/TenantTableHelper.cs
@@ -4,38 +4,46 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos.Table;
 using tenant_manager.Models;
+using Microsoft.Azure.Documents;
 
 namespace tenant_manager.Helpers
 {
     public class TenantTableHelper
     {
-        public static async void WriteNewTenantToTableAsync(string storageAccountConnectionString, string tableName, TenantModel tenant)
+        public static async Task WriteNewTenantToTableAsync(string storageAccountConnectionString, string tableName, TenantModel tenant)
         {
             /* Writes a new tenant object to a storage table */
 
-            // Get a reference to the storage table
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
-            CloudTableClient client = storageAccount.CreateCloudTableClient();
-            CloudTable table = client.GetTableReference(tableName);
+                // Get a reference to the storage table
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+                CloudTableClient client = storageAccount.CreateCloudTableClient();
+                CloudTable table = client.GetTableReference(tableName);
+            try
+            {
+                // Create the table if it doesn't already exist
+                await table.CreateIfNotExistsAsync();
 
-            // Create the table if it doesn't already exist
-            await table.CreateIfNotExistsAsync();
-
-            // Create and save the new tenant
-            TableOperation insertOp = TableOperation.Insert(tenant);
-            await table.ExecuteAsync(insertOp);
+                // Create and save the new tenant
+                TableOperation insertOp = TableOperation.Insert(tenant);
+                TableResult tableResult = await table.ExecuteAsync(insertOp);
+            }
+            catch (StorageException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
         }
 
         public static async Task<TenantModel> ReadTenantFromTableAsync(string storageAccountConnectionString, string tableName, string tenantId)
         {
             /* Return a tenant from table storage */
-
+            try { 
             // Get a reference to the storage table
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
             CloudTableClient client = storageAccount.CreateCloudTableClient();
@@ -45,9 +53,52 @@ namespace tenant_manager.Helpers
             string partitionKey = tenantId.Substring(0, 1);
             TableOperation readOp = TableOperation.Retrieve<TenantModel>(partitionKey, tenantId);
             TableResult tableResult = await table.ExecuteAsync(readOp);
-            TenantModel result = (TenantModel) tableResult.Result;
-
+            TenantModel result = tableResult.Result as TenantModel;
+            if (result != null)
+            {
+                Console.WriteLine("\t{0}\t{1}, result.PartitionKey, result.RowKey");
+            }
             return result;
         }
+        catch (StorageException e)
+        {
+           Console.WriteLine(e.Message);
+           Console.ReadLine();
+           throw;
+        }
+}
+
+
+
+        public static async Task DeleteEntityAsync(string storageAccountConnectionString, string tableName, TenantModel deleteEntity)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            CloudTableClient client = storageAccount.CreateCloudTableClient();
+            CloudTable table = client.GetTableReference(tableName);
+            try
+            {
+                if (deleteEntity == null)
+                {
+                    throw new ArgumentNullException("deleteEntity");
+                }
+
+                TableOperation deleteOperation = TableOperation.Delete(deleteEntity);
+                TableResult result = await table.ExecuteAsync(deleteOperation);
+
+                // Get the request units consumed by the current operation. RequestCharge of a TableResult is only applied to Azure CosmoS DB 
+                if (result.RequestCharge.HasValue)
+                {
+                    Console.WriteLine("Request Charge of Delete Operation: " + result.RequestCharge);
+                }
+
+            }
+            catch (StorageException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+
     }
 }
