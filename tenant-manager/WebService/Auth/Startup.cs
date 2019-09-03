061bd7042e4f298337aa6c88752748e34240168f
Nathan Oelke <a8pqnzz@mmm.com>
2019-09-03 14:36:54 -0500

    Added slash to auth url

--- tenant-manager/WebService/Auth/Startup.cs
+++ tenant-manager/WebService/Auth/Startup.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Azure.IoTSolutions.TenantManager.WebService.Auth
             }
 
             return new ConfigurationManager<OpenIdConnectConfiguration>(
-                config.ClientAuthConfig.JwtIssuer + ".well-known/openid-configuration",
+                config.ClientAuthConfig.JwtIssuer + "/.well-known/openid-configuration",
                 new OpenIdConnectConfigurationRetriever())
             {
                 // How often the list of keys in memory is refreshed. Default is 24 hours.
