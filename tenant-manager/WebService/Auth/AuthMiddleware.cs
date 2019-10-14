886da789716307eff761c771af8af6d78f705d30
Joe Bethke <a9q25zz@mmm.com>
2019-10-14 10:06:11 -0500

    fixing missing class attribute

--- tenant-manager/WebService/Auth/AuthMiddleware.cs
+++ tenant-manager/WebService/Auth/AuthMiddleware.cs
@@ -57,6 +57,7 @@ namespace MMM.Azure.IoTSolutions.TenantManager.WebService
         private TokenValidationParameters tokenValidationParams;
         private readonly bool authRequired;     
         private bool tokenValidationInitialized;
+        private readonly IServicesConfig servicesConfig;
         private readonly List<string> allowedUrls = new List<string>() { "/v1/status" };
 
         public AuthMiddleware(
