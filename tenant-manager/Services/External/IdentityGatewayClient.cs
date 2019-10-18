c396b625e2f9c0951777773c1534b6a883b3a976
Joe Bethke <a9q25zz@mmm.com>
2019-10-18 10:33:34 -0500

    Updating IDG client CreateRequest to always attach a tenantId in the headers. Adding docstrings.

--- tenant-manager/Services/External/IdentityGatewayClient.cs
+++ tenant-manager/Services/External/IdentityGatewayClient.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.IoTSolutions.Auth;
 using MMM.Azure.IoTSolutions.TenantManager.Services.Http;
 using HttpRequest = MMM.Azure.IoTSolutions.TenantManager.Services.Http.HttpRequest;
 using ILogger = MMM.Azure.IoTSolutions.TenantManager.Services.Diagnostics.ILogger;
@@ -29,9 +30,13 @@ namespace MMM.Azure.IoTSolutions.TenantManager.Services.External
             this._httpContextAccessor = httpContextAccessor;
         }
 
+        /// <summary>
+        /// Ping the IdentityGateway for its status
+        /// </summary>
+        /// <returns></returns>
         public async Task<StatusResultServiceModel> StatusAsync()
         {
-            HttpRequest request = CreateRequest("status/", new IdentityGatewayApiModel() { });
+            HttpRequest request = CreateRequest("status/");
             try
             {
                 StatusServiceModel result = await this.processApiModelRequest<StatusServiceModel>(this._httpClient.GetAsync, request);
@@ -55,45 +60,124 @@ namespace MMM.Azure.IoTSolutions.TenantManager.Services.External
             }
         }
 
+        /// <summary>
+        /// Add a user-tenant relationship between the given userId and tenantId, with the given roles
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="tenantId"></param>
+        /// <param name="Roles"></param>
+        /// <returns></returns>
         public async Task<IdentityGatewayApiModel> addTenantForUserAsync(string userId, string tenantId, string Roles)
         {
-            HttpRequest request = CreateRequest($"tenants/{userId}", new IdentityGatewayApiModel(Roles) { });
-            request.Headers.Add(TENANT_HEADER, tenantId);
+            HttpRequest request = CreateRequest($"tenants/{userId}", tenantId);
+            request.SetContent(new IdentityGatewayApiModel(Roles));
             return await this.processApiModelRequest<IdentityGatewayApiModel>(this._httpClient.PostAsync, request);
         }
 
+        /// <summary>
+        /// get a user-tenant relationship for the given user and tenant Ids
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="tenantId"></param>
+        /// <returns></returns>
         public async Task<IdentityGatewayApiModel> getTenantForUserAsync(string userId, string tenantId)
         {
-            HttpRequest request = CreateRequest($"tenants/{userId}", new IdentityGatewayApiModel { });
-            request.Headers.Add(TENANT_HEADER, tenantId);
+            HttpRequest request = CreateRequest($"tenants/{userId}", tenantId);
             return await this.processApiModelRequest<IdentityGatewayApiModel>(this._httpClient.GetAsync, request);
         }
         
+        /// <summary>
+        /// delete all user-tenant relationships for the given tenantId
+        /// </summary>
+        /// <param name="tenantId"></param>
+        /// <returns></returns>
         public async Task<IdentityGatewayApiModel> deleteTenantForAllUsersAsync(string tenantId)
         {
-            HttpRequest request = CreateRequest($"tenants/", new IdentityGatewayApiModel { });
-            request.Headers.Add(TENANT_HEADER, tenantId);
+            HttpRequest request = CreateRequest($"tenants/all", tenantId);
             return await this.processApiModelRequest<IdentityGatewayApiModel>(this._httpClient.DeleteAsync, request);
         }
 
+        /// <summary>
+        /// get the userSetting for the given settingKey
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="settingKey"></param>
+        /// <returns></returns>
         public async Task<IdentityGatewayApiSettingModel> getSettingsForUserAsync(string userId, string settingKey)
         {
-            HttpRequest request = CreateRequest($"settings/{userId}/{settingKey}", new IdentityGatewayApiModel { });
+            HttpRequest request = CreateRequest($"settings/{userId}/{settingKey}");
             return await this.processApiModelRequest<IdentityGatewayApiSettingModel>(this._httpClient.GetAsync, request);
         }
 
+        /// <summary>
+        /// Create a new userSetting of the given key and value
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="settingKey"></param>
+        /// <param name="settingValue"></param>
+        /// <returns></returns>
         public async Task<IdentityGatewayApiSettingModel> addSettingsForUserAsync(string userId, string settingKey, string settingValue)
         {
-            HttpRequest request = CreateRequest($"settings/{userId}/{settingKey}/{settingValue}", new IdentityGatewayApiModel { });
+            HttpRequest request = CreateRequest($"settings/{userId}/{settingKey}/{settingValue}");
             return await this.processApiModelRequest<IdentityGatewayApiSettingModel>(this._httpClient.GetAsync, request);
         }
 
+        /// <summary>
+        /// Change a userSetting for the given key, to the given value
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="settingKey"></param>
+        /// <param name="settingValue"></param>
+        /// <returns></returns>
         public async Task<IdentityGatewayApiSettingModel> updateSettingsForUserAsync(string userId, string settingKey, string settingValue)
         {
-            HttpRequest request = CreateRequest($"settings/{userId}/{settingKey}/{settingValue}", new IdentityGatewayApiModel { });
+            HttpRequest request = CreateRequest($"settings/{userId}/{settingKey}/{settingValue}");
             return await this.processApiModelRequest<IdentityGatewayApiSettingModel>(this._httpClient.PutAsync, request);
         }
 
+        /// <summary>
+        /// Create an HttpRequest with the necessary parameters for an IdentityGateway API request
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="tenantId"></param>
+        /// <returns></returns>
+        private HttpRequest CreateRequest(string path, string tenantId = null)
+        {
+            var request = new HttpRequest();
+            request.SetUriFromString($"{this.serviceUri}/{path}");
+            
+            string headerTenantId = tenantId ?? this._httpContextAccessor.HttpContext.Request.GetTenant();
+            Console.WriteLine(headerTenantId);
+            request.AddHeader(TENANT_HEADER, headerTenantId);
+
+            if (this.serviceUri.ToLowerInvariant().StartsWith("https:"))
+            {
+                request.Options.AllowInsecureSSLServer = true;
+            }
+
+            if (this._httpContextAccessor.HttpContext.Request.Headers.ContainsKey(AZDS_ROUTE_KEY))
+            {
+                try
+                {
+                    var azdsRouteAs = this._httpContextAccessor.HttpContext.Request.Headers.First(p => String.Equals(p.Key, AZDS_ROUTE_KEY, StringComparison.OrdinalIgnoreCase));
+                    request.Headers.Add(AZDS_ROUTE_KEY, azdsRouteAs.Value.First());  // azdsRouteAs.Value returns an iterable of strings, take the first
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Unable to attach the {AZDS_ROUTE_KEY} header to the IdentityGatewayClient Request.", e);
+                }
+            }
+
+            return request;
+        }
+
+        /// <summary>
+        /// Process an IdentityGateway request for the IdentityGateway and transform the response to Model T
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="request"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         private async Task<T> processApiModelRequest<T>(requestMethod method, HttpRequest request)
         {
             IHttpResponse response = null;
@@ -123,37 +207,6 @@ namespace MMM.Azure.IoTSolutions.TenantManager.Services.External
             {
                 throw new JsonReaderException("Unable to deserialize response content to the proper API model.", e);
             }
-        }
-
-        private HttpRequest CreateRequest(string path, IdentityGatewayApiModel content)
-        {
-            var request = new HttpRequest();
-            request.SetUriFromString($"{this.serviceUri}/{path}");
-
-            if (this.serviceUri.ToLowerInvariant().StartsWith("https:"))
-            {
-                request.Options.AllowInsecureSSLServer = true;
-            }
-
-            if (content != null)
-            {
-                request.SetContent(content);
-            }
-
-            if (this._httpContextAccessor.HttpContext.Request.Headers.ContainsKey(AZDS_ROUTE_KEY))
-            {
-                try
-                {
-                    var azdsRouteAs = this._httpContextAccessor.HttpContext.Request.Headers.First(p => String.Equals(p.Key, AZDS_ROUTE_KEY, StringComparison.OrdinalIgnoreCase));
-                    request.Headers.Add(AZDS_ROUTE_KEY, azdsRouteAs.Value.First());  // azdsRouteAs.Value returns an iterable of strings, take the first
-                }
-                catch (Exception e)
-                {
-                    throw new Exception($"Unable to attach the {AZDS_ROUTE_KEY} header to the IdentityGatewayClient Request.", e);
-                }
-            }
-
-            return request;
         }
     }
 }
