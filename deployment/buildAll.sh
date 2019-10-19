aa3b2e09ff387658741b348b259d03023a22ca4e
Andrew Schmidt <Andrew.mkniger@gmail.com>
2019-10-19 01:26:28 -0500

    fix storage-adapter

---
+++ deployment/buildAll.sh
@@ -0,0 +1,7 @@
+./runPipeline.sh config-build
+./runPipeline.sh device-telemetry-build
+./runPipeline.sh iothub-manager-build
+./runPipeline.sh identity-gateway-build
+./runPipeline.sh storage-adapter-build
+./runPipeline.sh reverse-proxy-build
+./runPipeline.sh webui-build
---
+++ deployment/runPipeline.sh
@@ -0,0 +1,1 @@
+az pipelines build queue --organization https://dev.azure.com/3M-Bluebird/ --project AzurePlatform --definition-name $1
