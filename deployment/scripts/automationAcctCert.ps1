8e0c2eda834f51c8cddefa20becefe84d7af7b25
Radharaman Mohanty CW <A84QHZZ@mmm.com>
2019-10-21 15:06:24 -0500

    changes to cosmos db

--- deployment/scripts/automationAcctCert.ps1
+++ deployment/scripts/automationAcctCert.ps1
@@ -94,14 +94,12 @@ function Get-RandomCharacters($length, $characters) {
     $private:ofs=""
     return [String]$characters[$random]
 }
-
 function Scramble-String([string]$inputString){     
     $characterArray = $inputString.ToCharArray()   
     $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
     $outputString = -join $scrambledStringArray
     return $outputString 
 }
-
 function Generate-Random-password() {
     $password = Get-RandomCharacters -length 1 -characters 'abcdefghiklmnoprstuvwxyz'
     $password += Get-RandomCharacters -length 2 -characters '1234567890'
