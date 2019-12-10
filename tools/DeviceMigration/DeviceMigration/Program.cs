ebac2f76ab4c38e7be682317050b8be6b972b04f
Swaminathan Sayee <a95lgzz@mmm.com>
2019-12-10 15:21:25 -0600

    Tool to migrate device twins from existing IoT

---
+++ tools/DeviceMigration/DeviceMigration/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace DeviceMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting the push of devices");
+            Utils ut = new Utils();
+            bool result = ut.SendTwinsToEH().GetAwaiter().GetResult();
+            if (result)
+            {
+                Console.WriteLine("Push completed successfully, Press Enter to continue..");
+            }
+            else
+            {
+                Console.WriteLine("Push completed with failures, Press Enter to continue..");
+            }
+            Console.ReadLine();
+        }
+    }
+}
