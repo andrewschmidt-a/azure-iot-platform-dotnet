8cc73ca3cb5c8a630b4430913b0f46c863e3d77f
Andrew Schmidt <aschmidt4@mmm.com>
2019-12-16 19:20:58 -0600

    Added Client Creds Flow

---
+++ identity-gateway/WebService/Models/ClientCredentialInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mmm.Platform.IoT.IdentityGateway.WebService.Models
+{
+    public class ClientCredentialInput
+    {
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
+        public string tenant { get; set; }
+    }
+}
