f3a4547952c15e6ea43a02420147e0c26de68888
Andrew <a90q9zz@mmm.com>
2020-01-15 12:50:32 -0600

    throw error on no tenants

--- identity-gateway/WebService/v1/Controllers/AuthorizeController.cs
+++ identity-gateway/WebService/v1/Controllers/AuthorizeController.cs
@@ -92,6 +92,17 @@ namespace Mmm.Platform.IoT.IdentityGateway.Controllers
                 return StatusCode(401, e.Message);
             }
 
+            UserTenantInput tenantInput = new UserTenantInput
+            {
+                UserId = input.client_id
+            };
+            UserTenantListModel tenantsModel = await this._userTenantContainer.GetAllAsync(tenantInput);
+            if(tenantsModel.models.Count == 0){
+                throw new Exception("Not granted access to any tenants");
+            }
+
+
+
             // if successful, then mint token
             var jwtHandler = new JwtSecurityTokenHandler();
             var claims = new List<Claim>();
