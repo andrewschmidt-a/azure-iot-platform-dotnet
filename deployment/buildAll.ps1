912b831092083405a838a2a6737195f05d31d138
Andrew Schmidt <aschmidt4@mmm.com>
2020-01-20 10:36:37 -0600

    added asa-manager-build to build scripts

--- deployment/buildAll.ps1
+++ deployment/buildAll.ps1
@@ -1,5 +1,6 @@
 
 ./runPipeline.ps1 webui-build
+./runPipeline.ps1 asa-manager-build
 ./runPipeline.ps1 config-build
 ./runPipeline.ps1 device-telemetry-build
 ./runPipeline.ps1 iothub-manager-build
