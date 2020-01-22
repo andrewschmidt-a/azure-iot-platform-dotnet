f3249596ffe30a8e052b07ae84d8c7c9fdbe67fe
Kyle Estes <kestes.cw@mmm.com>
2020-01-22 09:03:42 -0600

    fix: broken unit test

--- identity-gateway/Services/Models/AuthState.cs
+++ identity-gateway/Services/Models/AuthState.cs
@@ -2,20 +2,28 @@
 // Copyright (c) 3M. All rights reserved.
 // </copyright>
 
+using Newtonsoft.Json;
+
 namespace Mmm.Iot.IdentityGateway.Services.Models
 {
     public class AuthState
     {
+        [JsonProperty("returnUrl")]
         public string ReturnUrl { get; set; }
 
+        [JsonProperty("state")]
         public string State { get; set; }
 
+        [JsonProperty("tenant")]
         public string Tenant { get; set; }
 
+        [JsonProperty("nonce")]
         public string Nonce { get; set; }
 
+        [JsonProperty("client_id")]
         public string ClientId { get; set; }
 
+        [JsonProperty("invitation")]
         public string Invitation { get; set; }
     }
 }
