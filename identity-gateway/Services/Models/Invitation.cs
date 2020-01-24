7223b4665982dd77cf075c033c43ed4346af830d
Kyle Estes <kestes.cw@mmm.com>
2020-01-24 14:27:56 -0600

    fix: ensure proper case is used when serializing and deserializing to JSON

--- identity-gateway/Services/Models/Invitation.cs
+++ identity-gateway/Services/Models/Invitation.cs
@@ -2,12 +2,16 @@
 // Copyright (c) 3M. All rights reserved.
 // </copyright>
 
+using Newtonsoft.Json;
+
 namespace Mmm.Iot.IdentityGateway.Services.Models
 {
     public class Invitation
     {
+        [JsonProperty("email_address")]
         public string EmailAddress { get; set; }
 
+        [JsonProperty("role")]
         public string Role { get; set; }
     }
 }
--- identity-gateway/WebService/Models/ClientCredentialInput.cs
+++ identity-gateway/WebService/Models/ClientCredentialInput.cs
@@ -2,14 +2,19 @@
 // Copyright (c) 3M. All rights reserved.
 // </copyright>
 
+using Newtonsoft.Json;
+
 namespace Mmm.Iot.IdentityGateway.WebService.Models
 {
     public class ClientCredentialInput
     {
+        [JsonProperty("client_id")]
         public string ClientId { get; set; }
 
+        [JsonProperty("client_secret")]
         public string ClientSecret { get; set; }
 
+        [JsonProperty("scope")]
         public string Scope { get; set; }
     }
 }
